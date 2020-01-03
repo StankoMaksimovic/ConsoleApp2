8da9c21b5d39820612f204240dbb9fb54a50ef1d
StankoMaksimovic <stankomaksimovic@icloud.com>
2020-01-03 19:01:38 +0100

    sdwa

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Evo nesto pise");
+            Console.WriteLine("Evo ne pise");
             Console.WriteLine("ala sam ja car");
         }
     }
