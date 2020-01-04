f22218336aa01b0cf7cacfdbb93eb8ad0c5a96aa
StankoMaksimovic <stankomaksimovic@icloud.com>
2020-01-04 11:39:23 +0100

    stanko je car

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Evo ne pise");
-            Console.WriteLine("ala sam ja car");
+            Console.WriteLine("ala jestivo ulje ja car");
         }
     }
 }
