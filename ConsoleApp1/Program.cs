5baff9948db60917de223217ebc975c5620c2d5b
StankoMaksimovic <stankomaksimovic@icloud.com>
2020-01-06 11:34:13 +0100

    ja sam legenda noci svetlsti dana

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Evo ne pise");
-            Console.WriteLine("ala jestivo ulje ja car");
+            Console.WriteLine("Gospodin mufasa");
         }
     }
 }
